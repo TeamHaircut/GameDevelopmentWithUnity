e6a60456e7e4195e0a6f9323ab5788221abf2ecd
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-01-10 14:11:55 -0500

    Add files via upload

---
+++ Crossfire3D/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float InterpolationRatio = 0.1f;
+    public int ballSpeed = 1000;
+    public GameObject ball;
+    public GameObject nozzle;
+
+    private Rigidbody rb;
+
+    private float movementX;
+    private float movementY;
+    private InputAction fireAction;
+
+    private void Awake()
+    {
+        var actions = GetComponent<PlayerInput>().actions;
+        fireAction = actions.FindAction("Fire");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnFire()
+    {
+        GameObject bullet = Instantiate(ball, nozzle.transform.position, Quaternion.identity) as GameObject;
+        bullet.GetComponent<Rigidbody>().AddForce(nozzle.transform.forward * ballSpeed);
+    }
+
+
+
+    private void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    private void Update()
+    {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), InterpolationRatio);
+    }
+
+    private void FixedUpdate()
+    {
+        //rb.AddForce(movement * speed);
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    Debug.Log("Here");
+        //    GameObject bullet = Instantiate(ball, transform.position, Quaternion.identity) as GameObject;
+        //    bullet.GetComponent<Rigidbody>().AddForce(transform.forward * 10);
+        //}
+    }
+
+}
---
+++ Crossfire3D/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Vector3 myVector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if(horizontal <0)
+        {
+            myVector = new Vector3(-1.0f, 0.0f, 0.0f);
+        }
+        else if(horizontal >0)
+        {
+            myVector = new Vector3(1.0f, 0.0f, 0.0f);
+        }
+        else
+        {
+            myVector = new Vector3(0.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey("left")||Input.GetKey("right"))
+        {
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(myVector), 0.035f);
+        }
+
+        Debug.Log(horizontal);
+    }
+}
