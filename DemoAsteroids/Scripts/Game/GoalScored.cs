91ec85392ee882976474ff6645667efd0d1ee169
Team Haircut Digital Network <TeamHaircutDigiNet@gmail.com>
2021-01-22 19:51:06 -0500

    Add files via upload

---
+++ DemoAsteroids/Scripts/Game/GoalScored.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Realtime;
+using Photon.Pun.UtilityScripts;
+
+using Photon.Pun;
+
+public class GoalScored : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Puck"))
+        {
+            //destroy current puck
+            //increase score
+            //goal fx
+
+            //start coundown for new round
+            //spawn new puck
+
+            //destory current puck
+            PhotonNetwork.Destroy(other.gameObject);
+
+            //increase score
+            if(this.gameObject.tag == "GoalZone0")
+            {
+                PhotonNetwork.PlayerList[0].SetScore(PhotonNetwork.PlayerList[0].GetScore()+1);
+            }
+            if (this.gameObject.tag == "GoalZone1")
+            {
+                PhotonNetwork.PlayerList[1].SetScore(PhotonNetwork.PlayerList[1].GetScore() + 1);
+            }
+
+            //if player0 or player1 score is < 3
+                //start coundown for new round
+
+                //spawn new puck
+                PhotonNetwork.InstantiateRoomObject("Puck", new Vector3(0.0f, 5.6f, 0.0f), Quaternion.Euler(0.0f, 0.0f, 0.0f), 0);
+            //else
+                //end game (show final score)
+                //return to lobby or start a new game
+
+        }
+
+    }
+}
---
+++ DemoAsteroids/Scripts/Game/Puck.cs
@@ -0,0 +1,75 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Asteroid.cs" company="Exit Games GmbH">
+//   Part of: Asteroid Demo
+// </copyright>
+// <summary>
+//  Asteroid Component
+// </summary>
+// <author>developer@exitgames.com</author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using UnityEngine;
+
+using Random = UnityEngine.Random;
+using Photon.Pun.UtilityScripts;
+
+namespace Photon.Pun.Demo.Asteroids
+{
+    public class Puck : MonoBehaviour
+    {
+        public bool isLargeAsteroid;
+
+        private bool isDestroyed;
+
+        private PhotonView photonView;
+
+#pragma warning disable 0109
+#pragma warning restore 0109
+
+        #region UNITY
+
+        public void Awake()
+        {
+            photonView = GetComponent<PhotonView>();
+        }
+
+        public void Update()
+        {
+            if (!photonView.IsMine)
+            {
+                return;
+            }
+
+            if (Mathf.Abs(transform.position.x) > Camera.main.orthographicSize * Camera.main.aspect || Mathf.Abs(transform.position.z) > Camera.main.orthographicSize)
+            {
+                // Out of the screen
+                PhotonNetwork.Destroy(gameObject);
+            }
+        }
+
+        public void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log(collision.gameObject);
+
+            if (collision.gameObject.CompareTag("GoalZone1"))
+            {
+                    DestroyAsteroidGlobally();
+                PhotonNetwork.InstantiateRoomObject("Puck", new Vector3(0.0f, 5.6f, 0.0f),Quaternion.Euler(0.0f, 0.0f, 0.0f), 0);
+            }
+        }
+
+        #endregion
+
+        private void DestroyAsteroidGlobally()
+        {
+            isDestroyed = true;
+            PhotonNetwork.Destroy(gameObject);
+        }
+
+        private void DestroyAsteroidLocally()
+        {
+            isDestroyed = true;
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
